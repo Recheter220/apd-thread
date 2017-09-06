7a8d1c8f0da418162598110095073bb6d2a6303f
Recheter220 <recheter220@gmail.com>
2017-09-06 17:23:24 -0300

    Add files via upload

--- APD.Projetos/APD/frmPrincipal.Designer.cs
+++ APD.Projetos/APD/frmPrincipal.Designer.cs
@@ -28,102 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pcbImagem1 = new System.Windows.Forms.PictureBox();
-            this.pcbImagem2 = new System.Windows.Forms.PictureBox();
-            this.pcbImagem3 = new System.Windows.Forms.PictureBox();
-            this.pcbImagem4 = new System.Windows.Forms.PictureBox();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.pcbLua = new System.Windows.Forms.PictureBox();
+            this.pcbMJ = new System.Windows.Forms.PictureBox();
+            this.pcbNeil = new System.Windows.Forms.PictureBox();
             this.btnAtualizar = new System.Windows.Forms.Button();
-            this.gpbImagens = new System.Windows.Forms.GroupBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem4)).BeginInit();
-            this.gpbImagens.SuspendLayout();
+            this.pnlGif = new System.Windows.Forms.Panel();
+            this.pnlTitulo = new System.Windows.Forms.Panel();
+            this.pnlDgv = new System.Windows.Forms.Panel();
+            this.dgvRender = new System.Windows.Forms.DataGridView();
+            this.dgv_colMj = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv_colNeil = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLua)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMJ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbNeil)).BeginInit();
+            this.pnlGif.SuspendLayout();
+            this.pnlTitulo.SuspendLayout();
+            this.pnlDgv.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRender)).BeginInit();
             this.SuspendLayout();
             // 
-            // pcbImagem1
+            // pcbLua
             // 
-            this.pcbImagem1.Location = new System.Drawing.Point(21, 29);
-            this.pcbImagem1.Name = "pcbImagem1";
-            this.pcbImagem1.Size = new System.Drawing.Size(158, 158);
-            this.pcbImagem1.TabIndex = 0;
-            this.pcbImagem1.TabStop = false;
+            this.pcbLua.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pcbLua.Location = new System.Drawing.Point(0, 0);
+            this.pcbLua.Name = "pcbLua";
+            this.pcbLua.Size = new System.Drawing.Size(401, 419);
+            this.pcbLua.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbLua.TabIndex = 0;
+            this.pcbLua.TabStop = false;
+            this.pcbLua.Click += new System.EventHandler(this.pcbLua_Click);
             // 
-            // pcbImagem2
+            // pcbMJ
             // 
-            this.pcbImagem2.Location = new System.Drawing.Point(185, 29);
-            this.pcbImagem2.Name = "pcbImagem2";
-            this.pcbImagem2.Size = new System.Drawing.Size(158, 158);
-            this.pcbImagem2.TabIndex = 1;
-            this.pcbImagem2.TabStop = false;
+            this.pcbMJ.Location = new System.Drawing.Point(42, 27);
+            this.pcbMJ.Name = "pcbMJ";
+            this.pcbMJ.Size = new System.Drawing.Size(150, 150);
+            this.pcbMJ.TabIndex = 1;
+            this.pcbMJ.TabStop = false;
             // 
-            // pcbImagem3
+            // pcbNeil
             // 
-            this.pcbImagem3.Location = new System.Drawing.Point(349, 29);
-            this.pcbImagem3.Name = "pcbImagem3";
-            this.pcbImagem3.Size = new System.Drawing.Size(158, 158);
-            this.pcbImagem3.TabIndex = 2;
-            this.pcbImagem3.TabStop = false;
-            // 
-            // pcbImagem4
-            // 
-            this.pcbImagem4.Location = new System.Drawing.Point(513, 29);
-            this.pcbImagem4.Name = "pcbImagem4";
-            this.pcbImagem4.Size = new System.Drawing.Size(158, 158);
-            this.pcbImagem4.TabIndex = 3;
-            this.pcbImagem4.TabStop = false;
+            this.pcbNeil.Location = new System.Drawing.Point(64, 194);
+            this.pcbNeil.Name = "pcbNeil";
+            this.pcbNeil.Size = new System.Drawing.Size(108, 111);
+            this.pcbNeil.TabIndex = 2;
+            this.pcbNeil.TabStop = false;
             // 
             // btnAtualizar
             // 
             this.btnAtualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnAtualizar.Location = new System.Drawing.Point(16, 234);
+            this.btnAtualizar.Location = new System.Drawing.Point(12, 8);
             this.btnAtualizar.Name = "btnAtualizar";
-            this.btnAtualizar.Size = new System.Drawing.Size(179, 62);
+            this.btnAtualizar.Size = new System.Drawing.Size(383, 62);
             this.btnAtualizar.TabIndex = 4;
-            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.Text = "EU ENTENDI A REFERÊNCIA!";
             this.btnAtualizar.UseVisualStyleBackColor = true;
             this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
             // 
-            // gpbImagens
+            // pnlGif
             // 
-            this.gpbImagens.Controls.Add(this.pcbImagem4);
-            this.gpbImagens.Controls.Add(this.pcbImagem3);
-            this.gpbImagens.Controls.Add(this.pcbImagem2);
-            this.gpbImagens.Controls.Add(this.pcbImagem1);
-            this.gpbImagens.Location = new System.Drawing.Point(16, 5);
-            this.gpbImagens.Name = "gpbImagens";
-            this.gpbImagens.Size = new System.Drawing.Size(691, 210);
-            this.gpbImagens.TabIndex = 5;
-            this.gpbImagens.TabStop = false;
-            this.gpbImagens.Text = "Imagens";
+            this.pnlGif.Controls.Add(this.pcbMJ);
+            this.pnlGif.Controls.Add(this.pcbNeil);
+            this.pnlGif.Controls.Add(this.pcbLua);
+            this.pnlGif.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlGif.Location = new System.Drawing.Point(0, 0);
+            this.pnlGif.Name = "pnlGif";
+            this.pnlGif.Size = new System.Drawing.Size(401, 419);
+            this.pnlGif.TabIndex = 5;
+            // 
+            // pnlTitulo
+            // 
+            this.pnlTitulo.Controls.Add(this.btnAtualizar);
+            this.pnlTitulo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlTitulo.Location = new System.Drawing.Point(0, 419);
+            this.pnlTitulo.Name = "pnlTitulo";
+            this.pnlTitulo.Size = new System.Drawing.Size(401, 78);
+            this.pnlTitulo.TabIndex = 6;
+            // 
+            // pnlDgv
+            // 
+            this.pnlDgv.Controls.Add(this.dgvRender);
+            this.pnlDgv.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnlDgv.Location = new System.Drawing.Point(401, 0);
+            this.pnlDgv.Name = "pnlDgv";
+            this.pnlDgv.Size = new System.Drawing.Size(316, 497);
+            this.pnlDgv.TabIndex = 3;
+            // 
+            // dgvRender
+            // 
+            this.dgvRender.AllowUserToAddRows = false;
+            this.dgvRender.AllowUserToDeleteRows = false;
+            this.dgvRender.AllowUserToResizeColumns = false;
+            this.dgvRender.AllowUserToResizeRows = false;
+            this.dgvRender.BackgroundColor = System.Drawing.SystemColors.ControlDark;
+            this.dgvRender.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRender.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv_colMj,
+            this.dgv_colNeil});
+            this.dgvRender.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvRender.GridColor = System.Drawing.Color.White;
+            this.dgvRender.Location = new System.Drawing.Point(0, 0);
+            this.dgvRender.Name = "dgvRender";
+            this.dgvRender.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dgvRender.Size = new System.Drawing.Size(316, 497);
+            this.dgvRender.TabIndex = 0;
+            this.dgvRender.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRender_CellClick);
+            this.dgvRender.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvRender_CellDoubleClick);
+            // 
+            // dgv_colMj
+            // 
+            this.dgv_colMj.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.dgv_colMj.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_colMj.HeaderText = "Michael Jackson";
+            this.dgv_colMj.Name = "dgv_colMj";
+            this.dgv_colMj.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_colMj.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // dgv_colNeil
+            // 
+            this.dgv_colNeil.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            this.dgv_colNeil.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_colNeil.HeaderText = "Neil";
+            this.dgv_colNeil.Name = "dgv_colNeil";
+            this.dgv_colNeil.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_colNeil.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(717, 377);
-            this.Controls.Add(this.gpbImagens);
-            this.Controls.Add(this.btnAtualizar);
+            this.ClientSize = new System.Drawing.Size(717, 497);
+            this.Controls.Add(this.pnlGif);
+            this.Controls.Add(this.pnlTitulo);
+            this.Controls.Add(this.pnlDgv);
             this.Name = "Form1";
-            this.Text = "Programação Paralela";
+            this.Text = "Neil the Tiger getting his arm strong while Michael Jackson moonwalks";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.frmPrincipal_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pcbImagem4)).EndInit();
-            this.gpbImagens.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLua)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbMJ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbNeil)).EndInit();
+            this.pnlGif.ResumeLayout(false);
+            this.pnlTitulo.ResumeLayout(false);
+            this.pnlDgv.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRender)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pcbImagem1;
-        private System.Windows.Forms.PictureBox pcbImagem2;
-        private System.Windows.Forms.PictureBox pcbImagem3;
-        private System.Windows.Forms.PictureBox pcbImagem4;
+        private System.Windows.Forms.PictureBox pcbLua;
+        private System.Windows.Forms.PictureBox pcbMJ;
+        private System.Windows.Forms.PictureBox pcbNeil;
         private System.Windows.Forms.Button btnAtualizar;
-        private System.Windows.Forms.GroupBox gpbImagens;
+        private System.Windows.Forms.Panel pnlGif;
+        private System.Windows.Forms.Panel pnlTitulo;
+        private System.Windows.Forms.Panel pnlDgv;
+        private System.Windows.Forms.DataGridView dgvRender;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_colMj;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_colNeil;
     }
 }
 
--- APD.Projetos/APD/frmPrincipal.cs
+++ APD.Projetos/APD/frmPrincipal.cs
@@ -19,131 +19,62 @@ namespace APD
             InitializeComponent();
         }
 
+        Bitmap[] mj;
+        Bitmap[] neil;
+        Bitmap moon;
 
-        public void Animacao1()
+        public Bitmap GetImage(string prefixoComExtensao, int? indice)
+        {
+            if (string.IsNullOrEmpty(indice.ToString()))
+            {
+                return (Bitmap)Image.FromFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\resources\" + prefixoComExtensao);
+            }
+            else
+            {
+                return (Bitmap)Image.FromFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\resources\" + prefixoComExtensao + indice + ".png");
+            }
+        }
+
+        public void Moonwalk()
         {
             try
             {
-                int i = 1;
-                bool pos = true;
-                while (true)
+                foreach (DataGridViewRow rw in dgvRender.Rows)
                 {
-                    Image img = Image.FromFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\resources\img_" + i + ".png");
-                    this.Refresh();
-                    PictureBox picBox = ((PictureBox)this.Controls.Find("pcbImagem1", true)[0]);
-                    //picBox.Invoke((Action)delegate
-                    pcbImagem1.Invoke((Action)delegate
+                    int i = Convert.ToInt32(dgvRender.Rows[rw.Index].Cells["dgv_colMj"].Value);
+                    if (i >= 1 && i <= 13)
+                    {
+                        pnlGif.Invoke((Action)delegate
                         {
-                            pcbImagem1.Image = img;
-                            Thread.Sleep(100);
-                            this.Refresh();
-                            this.DestroyHandle();
+                            pcbMJ.Image = mj[i - 1];
+                            pcbMJ.Location = new Point(pcbLua.Width - (15 * rw.Index), pcbMJ.Location.Y);
+                            pcbMJ.Refresh();
                         });
-                    /*
-                    pcbImagem2.Invoke((Action)delegate
-                    {
-                        pcbImagem2.Image = img;
-                        Thread.Sleep(100);
-                        this.Refresh();
-                    });*/
-
-
-                    if (pos)
-                    {
-                        if (i < 6)
-                        {
-                            i++;
-                        }
-                        else
-                        {
-                            pos = false;
-                        }
                     }
-                    else
-                    {
-                        if (i > 1)
-                        {
-                            i--;
-                        }
-                        else
-                        {
-                            pos = true;
-                        }
-                    }
+                    Thread.Sleep(66);
                 }
             }
-
             catch (Exception mess)
             {
                 MessageBox.Show(mess.ToString(), "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        public void Animacao2()
+        public void Pushups()
         {
             try
             {
-                int i = 1;
-                bool pos = true;
-                for (i = 1; i <= 6; i++)
+                foreach (DataGridViewRow rw in dgvRender.Rows)
                 {
-                    Image img = Image.FromFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\resources\img2_" + i + ".png");
-                    this.Refresh();
-                    PictureBox picBox = ((PictureBox)this.Controls.Find("pcbImagem2", true)[0]);
-                    pcbImagem2.Invoke((Action)delegate
+                    int i = Convert.ToInt32(dgvRender.Rows[rw.Index].Cells["dgv_colNeil"].Value);
+                    if (i >= 1 && i <= 6)
                     {
-                        pcbImagem2.Image = img;
-                        Thread.Sleep(100);
-                        this.Refresh();
-                        this.DestroyHandle();
-                    });
-                }
-            }
-
-            catch (Exception mess)
-            {
-                MessageBox.Show(mess.ToString(), "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        public void Animacao3()
-        {
-            try
-            {
-                int i = 1;
-                bool pos = true;
-                while (true)
-                {
-                    Image img = Image.FromFile(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + @"\resources\img_" + i + ".png");
-                    this.Refresh();
-                    PictureBox picBox = ((PictureBox)this.Controls.Find("pcbImagem3", true)[0]);
-                    picBox.Invoke((Action)delegate
-                    {
-                        picBox.Image = img;
-                        Thread.Sleep(100);
-                        this.Refresh();
-                    });
-                    if (pos)
-                    {
-                        if (i < 6)
+                        pnlGif.Invoke((Action)delegate
                         {
-                            i++;
-                        }
-                        else
-                        {
-                            pos = false;
-                        }
-                    }
-                    else
-                    {
-                        if (i > 1)
-                        {
-                            i--;
-                        }
-                        else
-                        {
-                            pos = true;
-                        }
+                            pcbNeil.Image = neil[i - 1];
+                            pcbNeil.Refresh();
+                            //Thread.Sleep(66);
+                        });
                     }
                 }
             }
@@ -156,33 +87,105 @@ namespace APD
 
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
-            pcbImagem1.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbImagem2.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbImagem3.SizeMode = PictureBoxSizeMode.StretchImage;
+            mj = new Bitmap[13];
+            for (int indice = 1; indice <= 13; indice++)
+            {
+                mj[indice - 1] = GetImage("mwk", indice);
+            }
+
+            neil = new Bitmap[6];
+            for (int indice = 1; indice <= 6; indice++)
+            {
+                neil[indice - 1] = GetImage("armstr", indice);
+            }
+
+            moon = GetImage("moon.jpg", null);
+
+            int posX = pcbLua.Width;
+            int frame = 1;
+            int row = 0;
+            while (posX > 0 - pcbMJ.Width)
+            {
+                dgvRender.Rows.Add();
+                dgvRender.Rows[row].Cells["dgv_colMj"].Value = frame;
+                row++;
+                posX -= 15;
+                if (frame < 13)
+                    frame++;
+                else
+                    frame = 1;
+            }
+            for (frame = 1; frame <= 6; frame++)
+            {
+                dgvRender.Rows[frame - 1].Cells["dgv_colNeil"].Value = frame;
+            }
+
+            pcbLua.Image = moon;
+            pcbLua.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pcbMJ.Image = mj[0];
+            pcbMJ.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbMJ.BackColor = Color.Transparent;
+            pcbMJ.Parent = pcbLua;
+            pcbMJ.Anchor = AnchorStyles.Right;
+            pcbMJ.Location = new Point(pcbLua.Width, pcbLua.Height - 320);
+
+            pcbNeil.Image = neil[0];
+            pcbNeil.SizeMode = PictureBoxSizeMode.StretchImage;
+            pcbNeil.BackColor = Color.Transparent;
+            pcbNeil.Parent = pcbLua;
+            pcbNeil.Location = new Point(((pcbLua.Width / 2) - (pcbNeil.Width / 2)), pcbLua.Height - 150);
         }
 
         private void btnAtualizar_Click(object sender, EventArgs e)
         {
-            pcbImagem1.Image = null;
-            pcbImagem2.Image = null;
-            pcbImagem3.Image = null;
+            //pcbLua.Image = null;
+            pcbMJ.Image = null;
+            pcbMJ.Location = new Point(pcbLua.Width, pcbLua.Height - 320);
+            pcbMJ.Refresh();
 
-            //// Setup the cancellation mechanism.
-            //var cts = new CancellationTokenSource();
-            //var po = new ParallelOptions();
-            //po.CancellationToken = cts.Token;
+            pcbNeil.Image = null;
+            pcbNeil.Location = new Point(((pcbLua.Width / 2) - (pcbNeil.Width / 2)), pcbLua.Height - 150);
+            pcbNeil.Refresh();
 
-            //Parallel.Invoke(po() => {
-            //    new Action(() => { Animacao1(); })
-            //        });
-
-            //);
             Parallel.Invoke(
-                new Action(() => { Animacao1(); }),
-                new Action(() => { Animacao2(); })//,
-                //new Action(() => { Animacao3(); })
+                new Action(() => { Moonwalk(); }),
+                new Action(() => { Pushups(); })
             );
 
+        }
+
+        private void pcbLua_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvRender_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void dgvRender_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dgvRender.Columns["dgv_colMj"].Index)
+            {
+                int posX = pcbLua.Width - (15 * e.RowIndex);
+                int posY = pcbMJ.Location.Y;
+                Point posicao = new Point(posX, posY);
+                pcbMJ.Location = posicao;
+
+                int frame = Convert.ToInt32(dgvRender.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                if (frame >= 1 && frame <= 13)
+                    pcbMJ.Image = mj[frame - 1];
+            }
+            else if (e.ColumnIndex == dgvRender.Columns["dgv_colNeil"].Index)
+            {
+                if (e.ColumnIndex == dgvRender.Columns["dgv_colNeil"].Index)
+                {
+                    int frame = Convert.ToInt32(dgvRender.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+                    if (frame >= 1 && frame <= 6)
+                        pcbNeil.Image = neil[frame - 1];
+                }
+            }
         }
     }
 }
